c4b4e90562c8af6b2aa4c6ceb5ed06ffa81616c7
Eri Wandri <wawandry@gmail.com>
2021-08-12 14:08:22 +0700

    Added transaction detail screen to navigator

---
+++ src/screens/Transaction/TransactionDetailScreen.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+import { Container } from '../../components/container'
+import { Text } from 'react-native'
+
+const TransactionDetailScreen: FC = () => {
+    return (
+        <Container>
+            <Text>Detail</Text>
+
+        </Container>
+    )
+}
+
+export default TransactionDetailScreen
