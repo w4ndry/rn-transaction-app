3609ef2bfd86eec40480d17a6a10398a06e2f05a
Eri Wandri <wawandry@gmail.com>
2021-08-12 22:07:58 +0700

    Move type to file

---
+++ src/screens/Transaction/types.tsx
@@ -0,0 +1,14 @@
+export declare interface TransactionItem {
+    id: string,
+    amount: number,
+    unique_code: number,
+    status: string,
+    sender_bank: string,
+    account_number: string,
+    beneficiary_name: string,
+    beneficiary_bank: string,
+    remark: string,
+    created_at: string,
+    completed_at: string,
+    fee: number,
+}
